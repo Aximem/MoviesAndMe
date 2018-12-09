af4fbdb168c9f3ddd3e2c920634e46e3b0d0c828
Maxime Charruel <m.charruel@gmail.com>
2018-12-09 12:41:25 +0100

    [ADD] P2CH2

--- Components/Search.js
+++ Components/Search.js
@@ -1,17 +1,28 @@
 // Components/Search.js
 
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
 
 class Search extends React.Component {
   render() {
     return (
-      <View>
-        <TextInput placeholder='Titre du film'/>
+      <View style={{ marginTop: 20 }}>
+        <TextInput style={styles.textinput} placeholder='Titre du film'/>
         <Button title='Rechercher' onPress={() => {}}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
 
 export default Search
